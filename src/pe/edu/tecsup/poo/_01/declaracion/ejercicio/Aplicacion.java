baf8ca94b9a3750a355f3130c57ff7ce00e1df06
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 11:26:35 -0500

    update

---
+++ src/pe/edu/tecsup/poo/_01/declaracion/ejercicio/Aplicacion.java
@@ -0,0 +1,24 @@
+package pe.edu.tecsup.poo._01.declaracion.ejercicio;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        // estudiante1 :  objeto, instancia
+
+        Persona estudiante1 = new Persona();
+        estudiante1.nombre = "Juan";
+        estudiante1.apellido = "Perez";
+
+        System.out.println("Nombre estudiante 1: " + estudiante1.nombre + " " + estudiante1.apellido);
+
+        Persona estudiante2 = new Persona();
+        estudiante2.nombre = "Ana";
+        estudiante2.apellido = "Gomez";
+        System.out.println("Nombre estudiante 2: " + estudiante2.nombre + " " + estudiante2.apellido);
+
+
+
+
+    }
+}
---
+++ src/pe/edu/tecsup/poo/_01/declaracion/ejercicio/Persona.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.poo._01.declaracion.ejercicio;
+
+public class Persona {
+    String nombre;
+    String apellido;
+    int edad;
+    String dni;
+}
