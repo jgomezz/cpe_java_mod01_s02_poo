1e005c92aa9686c8db4912a318f39a7d74fe1a19
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 12:17:16 -0500

    update

--- src/pe/edu/tecsup/poo/_03/metodos/ejercicio/Aplicacion.java
+++ src/pe/edu/tecsup/poo/_03/metodos/ejercicio/Aplicacion.java
@@ -4,9 +4,29 @@ public class Aplicacion {
 
     public static void main(String[] args) {
 
-        CuentaBancaria cb = new CuentaBancaria();
-        cb.nroCuenta= "4567897987";
+        CuentaBancaria cuenta = new CuentaBancaria();
 
+        cuenta.numeroCuenta = "4567897987";
+        cuenta.nombreTitular = "Juan Perez";
+        cuenta.saldo = 5000;
+
+        // Mostrar los datos de la cuenta
+        String nroCuenta = cuenta.obtenerNumeroCuenta();
+        System.out.println("Numero de cuenta: " + nroCuenta);
+
+        // Realizar un pago de 100 soles
+        double pagoLuz = 100;
+        cuenta.realizarPago(pagoLuz);
+
+        // Obtener el saldo actual
+        double saldoActual = cuenta.obtenerSaldoActual();
+
+        System.out.printf("Saldo actual S/%.2f  \n" , saldoActual);
+
+        // Depositar 2000 soles a tu cuenta y obtener el nuevo saldo
+        cuenta.depositar();
+
+        //System.out.println(cuenta);
 
 
     }
--- src/pe/edu/tecsup/poo/_03/metodos/ejercicio/CuentaBancaria.java
+++ src/pe/edu/tecsup/poo/_03/metodos/ejercicio/CuentaBancaria.java
@@ -1,7 +1,34 @@
 package pe.edu.tecsup.poo._03.metodos.ejercicio;
 
 public class CuentaBancaria {
-    String nroCuenta;
+    String numeroCuenta;
     String nombreTitular;
     double saldo;
+
+    public String obtenerNumeroCuenta() {
+        return this.numeroCuenta;
+    }
+
+    public double obtenerSaldoActual(){
+        return this.saldo;
+    }
+
+    public void realizarPago( double montoPago){
+        this.saldo -= montoPago;
+    }
+
+    public void depositar(){
+        this.saldo += 2000;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Numero de cuenta: '" + numeroCuenta + '\'' +
+                ", nombre del titular: '" + nombreTitular + '\'' +
+                ", saldo: " + saldo;
+    }
+
+
+
 }
