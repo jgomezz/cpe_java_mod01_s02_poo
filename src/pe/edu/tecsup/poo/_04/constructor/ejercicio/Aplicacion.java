e6a3e9de3582e07ca8bb518a0072d8c796af9054
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 12:25:38 -0500

    update

---
+++ src/pe/edu/tecsup/poo/_04/constructor/ejercicio/Aplicacion.java
@@ -0,0 +1,33 @@
+package pe.edu.tecsup.poo._03.metodos.ejercicio;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        CuentaBancaria cuenta = new CuentaBancaria();
+
+        cuenta.numeroCuenta = "4567897987";
+        cuenta.nombreTitular = "Juan Perez";
+        cuenta.saldo = 5000;
+
+        // Mostrar los datos de la cuenta
+        String nroCuenta = cuenta.obtenerNumeroCuenta();
+        System.out.println("Numero de cuenta: " + nroCuenta);
+
+        // Realizar un pago de 100 soles
+        double pagoLuz = 100;
+        cuenta.realizarPago(pagoLuz);
+
+        // Obtener el saldo actual
+        double saldoActual = cuenta.obtenerSaldoActual();
+
+        System.out.printf("Saldo actual S/%.2f  \n" , saldoActual);
+
+        // Depositar 2000 soles a tu cuenta y obtener el nuevo saldo
+        cuenta.depositar();
+
+        //System.out.println(cuenta);
+
+
+    }
+}
---
+++ src/pe/edu/tecsup/poo/_04/constructor/ejercicio/README.md
@@ -0,0 +1,16 @@
+
+### Ejercicio :  Se tiene una cuenta Bancaria con la siguiente información:
+- Número de cuenta
+- Nombre del titular
+- Saldo
+
+Inicializar los datos
+- Numero de cuenta: 4567897987
+- Titular: Juan Perez
+- Saldo: 5000
+
+Crear los siguientes métodos:
+- Mostrar los datos de la cuenta
+- Realizar un pago de 100 soles
+- Depositar 2000 soles a tu cuenta y obtener el nuevo saldo
+- Obtener el saldo actual
