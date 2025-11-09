7abaa8fcb05603dc5549d3763b7f3cbae2734cce
Developer <developer@Developers-MacBook-Pro.local>
2025-11-09 08:30:21 -0500

    update

---
+++ src/pe/edu/tecsup/poo/_08/constantes/ejercicio/Aplicacion.java
@@ -0,0 +1,17 @@
+package pe.edu.tecsup.poo._08.constantes.ejercicio;
+
+/**
+ *  Crear el metodo estatico sumar, que permita sumar 2 enteros y retorne el resultado.
+ *  Crear el metodo restar, que permita restar 2 enteros y retorne el resultado.
+ *  Ambos metodos deben estar creados en la clase Calculadora.
+ *  En la clase Calcular definir la constante PI con el valor 3.1416
+ *  y usarla para calcular el area de un circulo.
+ *
+ */
+public class Aplicacion {
+
+
+    public static void main(String[] args) {
+
+    }
+}
