ac87ff014ce943aaef04d2bc7dceee131b6e0154
Developer <developer@Developers-MacBook-Pro.local>
2025-11-09 09:33:06 -0500

    update

--- src/pe/edu/tecsup/poo/_09/herencia/caso0/Aplicacion.java
+++ src/pe/edu/tecsup/poo/_09/herencia/caso0/Aplicacion.java
@@ -3,8 +3,13 @@ package pe.edu.tecsup.poo._09.herencia.caso0;
 public class Aplicacion {
 
     public static void main(String[] args) {
+
         WebPassword wp = new WebPassword();
-        wp.procesar();
+        String msg = "Enviame tu DNI";
+        String encrypted = wp.procesar(msg);
+        System.out.println("Mensaje Encriptado: " + encrypted);
+
+
     }
 
 }
--- src/pe/edu/tecsup/poo/_09/herencia/caso0/WebPassword.java
+++ src/pe/edu/tecsup/poo/_09/herencia/caso0/WebPassword.java
@@ -2,7 +2,7 @@ package pe.edu.tecsup.poo._09.herencia.caso0;
 
 public class WebPassword extends GeneralPassword {
 
-    public void procesar(){
+    public String procesar(String msg){
 
         //String k = keyword;
 
@@ -12,11 +12,11 @@ public class WebPassword extends GeneralPassword {
         //System.out.println("Keyword: " + k);
         System.out.println("Version: " + v);
 
-        String msg = "Enviame tu DNI";
 
         String encrypted = super.encrytarWord(msg);
 
-        System.out.println("Mensaje Encriptado: " + encrypted);
+
+        return encrypted;
     }
 
 }
